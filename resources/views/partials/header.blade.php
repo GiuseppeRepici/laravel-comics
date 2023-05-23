4a672f4875f6f1f7009de2115cf00a309c16ef03
GiuseppeRepici <giuseppe.repici95@gmail.com>
2023-05-23 16:29:47 +0200

    per oggi finisco qui , sono impegnato

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,17 +1,43 @@
 <nav class="bg-body-tertiary">
     <div class="container-fluid text-center">
-        <div id="navbarNav" class="justify-content-center d-flex">
-            <ul class="list-unstyled d-flex">
-                <li class="nav-item mx-3">
-                    <a class="nav-link active" aria-current="page" href="{{url('/')}}">Home</a>
-                </li>
-                <li class="nav-item mx-3">
-                    <a class="nav-link" href="">About</a>
-                </li>
-                <li class="nav-item mx-3">
-                    <a class="nav-link" href="">Blog</a>
-                </li>
-            </ul>
+        <div class="justify-content-center d-flex">
+            <div class="my-4">
+                <img class="logo" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="">
+            </div>
+            <div id="navbarNav" class="justify-content-center d-flex">
+                <ul class="list-unstyled d-flex my-5">
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="">CHARACTERS</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link active" aria-current="page" href="{{url('/')}}">COMICS</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="">MOVIES</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="">TV</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="">GAMES</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="">COLLECTIBLES</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="">VIDEOS</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="">FANS</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="">NEWS</a>
+                    </li>
+                    <li class="nav-item mx-3">
+                        <a class="nav-link" href="">SHOP</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="pasta-container py-4">
+    <div class="comics-container py-4">
         <div class="container">
             <h2>comics</h2>
             <div class="row row-cols-6 g-3">
                 @foreach ($comics as $comic)
                     <div class="col">
-                        <div class="card">
-                            <img src="{{ $comic['thumb'] }}" alt="">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $comic['series'] }}</h5>
+                        <div>
+                            <img class="foto" src="{{ $comic['thumb'] }}" alt="">
+                            <div>
+                                <h5>{{ $comic['series'] }}</h5>
                             </div>
                         </div>
                     </div>
                 @endforeach
             </div>
-
         </div>
     </div>
 @endsection
